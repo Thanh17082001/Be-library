ec066b57051c4605ceb62038c2531bd8f24e2a4b
Thanh17082001 <thienthanh17082001@gmail.com>
2024-09-20 17:11:45 +0700

    fix: swagger

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,20 @@ import { TransformInterceptor } from './interception/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api'); // set global route
   // config swagger
   const config = new DocumentBuilder()
     .setTitle('API USING NEST JS APP TIVI')
     .setDescription('Author: Nguyen Thien Thanh')
     .setVersion('1.0')
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
       tagsSorter: 'alpha', // Sắp xếp các tag theo thứ tự từ A-Z
+      persistAuthorization: true,
     },
   });
   // middleware
@@ -41,7 +42,7 @@ async function bootstrap() {
    const configService = app.get(ConfigService);
    const PORT = configService.get<number>('PORT') || 3000;
   await app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}/api`);
   });
 }
 bootstrap();
