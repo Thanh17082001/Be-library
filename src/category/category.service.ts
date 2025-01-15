53c2086f7f78a53cb61456bb963d19636468e3c4
Thanh17082001 <thienthanh17082001@gmail.com>
2025-01-15 17:10:32 +0700

    fix:category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,25 +1,25 @@
-import {CreateCategoryDto} from './dto/create-category.dto';
-import {UpdateCategoryDto} from './dto/update-category.dto';
-import {Injectable, NotFoundException, BadRequestException, HttpException} from '@nestjs/common';
-import {InjectModel} from '@nestjs/mongoose';
-import {Model, ObjectId, Types} from 'mongoose';
-import {PageOptionsDto} from 'src/utils/page-option-dto';
-import {ItemDto, PageDto} from 'src/utils/page.dto';
-import {PageMetaDto} from 'src/utils/page.metadata.dto';
-import {Category} from './entities/category.entity';
-import {SoftDeleteModel} from 'mongoose-delete';
-import {Group} from 'src/group/entities/group.entity';
-import {Type} from 'class-transformer';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Injectable, NotFoundException, BadRequestException, HttpException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId, Types } from 'mongoose';
+import { PageOptionsDto } from 'src/utils/page-option-dto';
+import { ItemDto, PageDto } from 'src/utils/page.dto';
+import { PageMetaDto } from 'src/utils/page.metadata.dto';
+import { Category } from './entities/category.entity';
+import { SoftDeleteModel } from 'mongoose-delete';
+import { Group } from 'src/group/entities/group.entity';
+import { Type } from 'class-transformer';
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectModel(Category.name) private categoryModel: SoftDeleteModel<Category>,
     @InjectModel(Group.name) private groupModel: SoftDeleteModel<Group>
-  ) {}
+  ) { }
   async create(createDto: CreateCategoryDto): Promise<Category> {
     createDto.name = createDto.name.toLowerCase();
-    const exits: Category = await this.categoryModel.findOne({name: createDto.name, libraryId: createDto.libraryId});
+    const exits: Category = await this.categoryModel.findOne({ name: createDto.name, libraryId: createDto.libraryId });
     if (exits) {
       throw new BadRequestException('name already exists');
     }
@@ -27,9 +27,9 @@ export class CategoryService {
   }
 
   async findAll(pageOptions: PageOptionsDto, query: Partial<Category>): Promise<PageDto<Category>> {
-    const {page, limit, skip, order, search} = pageOptions;
+    const { page, limit, skip, order, search } = pageOptions;
     const pagination = ['page', 'limit', 'skip', 'order', 'search'];
-    const mongoQuery: any = {isActive: 1};
+    const mongoQuery: any = { isActive: 1 };
     // Thêm các điều kiện từ `query`
     if (!!query && Object.keys(query).length > 0) {
       const arrayQuery = Object.keys(query);
@@ -42,7 +42,7 @@ export class CategoryService {
 
     //search document
     if (search) {
-      mongoQuery.name = {$regex: new RegExp(search, 'i')};
+      mongoQuery.name = { $regex: new RegExp(search, 'i') };
     }
 
     if (Object.keys(mongoQuery).includes('libraryId')) {
@@ -54,7 +54,7 @@ export class CategoryService {
       this.categoryModel
         .find(mongoQuery)
         // .populate('aaaaaa')
-        .sort({order: 1, createdAt: order === 'ASC' ? 1 : -1})
+        .sort({ name: 1 })
         .skip(skip)
         .limit(limit)
         .lean()
@@ -78,7 +78,7 @@ export class CategoryService {
   }
 
   async findByName(names: Array<string>, libraryId: string): Promise<Array<Types.ObjectId>> {
-    const existingResources = await this.categoryModel.find({name: {$in: names}, libraryId: new Types.ObjectId(libraryId)}).lean();
+    const existingResources = await this.categoryModel.find({ name: { $in: names }, libraryId: new Types.ObjectId(libraryId) }).lean();
 
     const existingNames = existingResources.map(item => item.name);
     const missingNames = names.filter(name => !existingNames.includes(name));
@@ -105,7 +105,7 @@ export class CategoryService {
     const exits: Category = await this.categoryModel.findOne({
       name: updateCategoryDto.name, // Tìm theo tên
       libraryId: new Types.ObjectId(updateCategoryDto.libraryId),
-      _id: {$ne: new Types.ObjectId(id)}, // Loại trừ ID hiện tại
+      _id: { $ne: new Types.ObjectId(id) }, // Loại trừ ID hiện tại
     });
     if (exits) {
       throw new BadRequestException('name already exists');
@@ -153,7 +153,7 @@ export class CategoryService {
   }
 
   async findDeleted(pageOptions: PageOptionsDto, query: Partial<Category>): Promise<PageDto<Category>> {
-    const {page, limit, skip, order, search} = pageOptions;
+    const { page, limit, skip, order, search } = pageOptions;
     const pagination = ['page', 'limit', 'skip', 'order', 'search'];
     const mongoQuery: any = {}; // Điều kiện để tìm các tài liệu đã bị xóa mềm
 
@@ -169,14 +169,14 @@ export class CategoryService {
 
     // Tìm kiếm tài liệu
     if (search) {
-      mongoQuery.name = {$regex: new RegExp(search, 'i')};
+      mongoQuery.name = { $regex: new RegExp(search, 'i') };
     }
 
     // Thực hiện phân trang và sắp xếp
     const [results, itemCount] = await Promise.all([
       this.categoryModel
         .findDeleted(mongoQuery) // Sử dụng phương thức `findDeleted` từ mongoose-delete
-        .sort({order: 1, createdAt: order === 'ASC' ? 1 : -1})
+        .sort({ order: 1, createdAt: order === 'ASC' ? 1 : -1 })
         .skip(skip)
         .limit(limit)
         .lean()
@@ -193,11 +193,11 @@ export class CategoryService {
   }
 
   async findByIdDeleted(id: Types.ObjectId): Promise<ItemDto<Category>> {
-    return new ItemDto(await this.categoryModel.findOneDeleted({_id: new Types.ObjectId(id)}));
+    return new ItemDto(await this.categoryModel.findOneDeleted({ _id: new Types.ObjectId(id) }));
   }
 
   async restoreById(id: string): Promise<Category> {
-    const restoredDocument = await this.categoryModel.restore({_id: id});
+    const restoredDocument = await this.categoryModel.restore({ _id: id });
 
     // Kiểm tra xem tài liệu đã được khôi phục hay không
     if (!restoredDocument) {
@@ -208,14 +208,14 @@ export class CategoryService {
   }
 
   async restoreByIds(ids: string[]): Promise<Category[]> {
-    const restoredDocuments = await this.categoryModel.restore({_id: {$in: ids}});
+    const restoredDocuments = await this.categoryModel.restore({ _id: { $in: ids } });
 
     // Kiểm tra xem có tài liệu nào được khôi phục hay không
     if (!restoredDocuments || restoredDocuments.length === 0) {
       throw new NotFoundException(`No documents found for the provided IDs`);
     }
 
-    await this.categoryModel.updateMany({_id: {$in: ids}}, {$set: {deleted: false}});
+    await this.categoryModel.updateMany({ _id: { $in: ids } }, { $set: { deleted: false } });
 
     return restoredDocuments;
   }
@@ -234,15 +234,15 @@ export class CategoryService {
   async deleteMultiple(ids: string[]): Promise<any> {
     const objectIds = ids.map(id => new Types.ObjectId(id));
     return await this.categoryModel.deleteMany({
-      _id: {$in: objectIds},
+      _id: { $in: objectIds },
     });
   }
 
   //liên thông
   async GetIsLink(libraryId: string, pageOptions: PageOptionsDto, query: Partial<Category>): Promise<any> {
-    const {page, limit, skip, order, search} = pageOptions;
+    const { page, limit, skip, order, search } = pageOptions;
     const group = await this.groupModel.findOne({
-      libraries: {$in: [libraryId]},
+      libraries: { $in: [libraryId] },
     });
     if (!group) {
       throw new BadRequestException('Thư viện chưa có trong nhóm');
@@ -252,9 +252,9 @@ export class CategoryService {
 
     // Thêm các điều kiện từ `query` và search
     const searchRegex = search
-      ? {$regex: search, $options: 'i'} // 'i' để không phân biệt hoa thường
+      ? { $regex: search, $options: 'i' } // 'i' để không phân biệt hoa thường
       : null;
-    const mongoQuery: any = {isLink: true};
+    const mongoQuery: any = { isLink: true };
     const pagination = ['page', 'limit', 'skip', 'order', 'search'];
     if (!!query && Object.keys(query).length > 0) {
       const arrayQuery = Object.keys(query);
@@ -268,7 +268,7 @@ export class CategoryService {
       mongoQuery.libraryId = new Types.ObjectId(mongoQuery.libraryId);
     }
     const match = {
-      ...(searchRegex && {name: searchRegex}),
+      ...(searchRegex && { name: searchRegex }),
       ...mongoQuery,
     };
 
@@ -276,7 +276,7 @@ export class CategoryService {
 
     // Xây dựng pipeline với điều kiện `$skip` và `$limit` động
     const pipeline: any[] = [
-      {$match: {...match}},
+      { $match: { ...match } },
       {
         $lookup: {
           from: 'libraries',
@@ -291,23 +291,23 @@ export class CategoryService {
           preserveNullAndEmptyArrays: true,
         },
       },
-      {$match: {'libraryDetails.groupId': {$exists: true, $eq: groupId}}},
-      {$sort: {createdAt: order === 'ASC' ? 1 : -1}},
+      { $match: { 'libraryDetails.groupId': { $exists: true, $eq: groupId } } },
+      { $sort: { createdAt: order === 'ASC' ? 1 : -1 } },
     ];
 
     // Thêm `$skip` và `$limit` vào pipeline nếu có giá trị
     if (typeof skip === 'number' && skip >= 0) {
-      pipeline.push({$skip: skip});
+      pipeline.push({ $skip: skip });
     }
     if (typeof limit === 'number' && limit > 0) {
-      pipeline.push({$limit: limit});
+      pipeline.push({ $limit: limit });
     }
 
     const results = await this.categoryModel.aggregate(pipeline);
 
     // Đếm tổng số tài liệu khớp với điều kiện
     const countPipeline = [
-      {$match: {...match}},
+      { $match: { ...match } },
       {
         $lookup: {
           from: 'libraries',
@@ -322,8 +322,8 @@ export class CategoryService {
           preserveNullAndEmptyArrays: true,
         },
       },
-      {$match: {'libraryDetails.groupId': groupId}},
-      {$count: 'totalCount'},
+      { $match: { 'libraryDetails.groupId': groupId } },
+      { $count: 'totalCount' },
     ];
 
     const countResult = await this.categoryModel.aggregate(countPipeline);
